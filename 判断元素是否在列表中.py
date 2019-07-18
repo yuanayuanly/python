bef1a37a8cc37ce60a25fafeaa951510d5f0de9e
yuanayuanly <50137765+yuanayuanly@users.noreply.github.com>
2019-07-18 18:35:34 +0800

    Add files via upload

---
+++ 判断元素是否在列表中.py
@@ -0,0 +1,7 @@
+# 在这里写上你的代码 :-)
+'''a = [1, 2, 3]
+print(1 in a)
+print(4 not in a)'''
+b =[4,5,6]
+print(7 in b)
+print(6 in b)
---
+++ 用for和range,创建一个包含整数的序列.py
@@ -0,0 +1,6 @@
+'''for i in range(5):
+    print(i)'''
+'''for a in range(1,5):
+    print(a)'''
+for b in range(50,55):
+    print(b)
